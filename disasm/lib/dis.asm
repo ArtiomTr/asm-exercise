2e749be35b8c3dee9b3defb53cbd31d053e688ce
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-12-21 18:39:00 +0200

    Added support for immediate arguments

--- disasm/lib/dis.asm
+++ disasm/lib/dis.asm
@@ -1,6 +1,17 @@
 CMD_SUB equ 0
+CMD_AAS equ 1
+CMD_DAS equ 2
+CMD_PUSH equ 3
+CMD_INC equ 4
+CMD_SHR equ 5
+CMD_ROR equ 6
+CMD_AAA equ 7
+CMD_DAA equ 8
+
 STATE_TWO_OPERANDS equ 1
 STATE_READ_OFFSET equ 2
+STATE_IMMEDIATE equ 3
+STATE_DEEP_2 equ 4
 
 ; ASCII symbols
 ASCII_CR equ 0Dh ; carriage return
@@ -22,7 +33,11 @@ LOCALS @@
     command db 0FFh
     state db 0
     _offset dw 0
+    immediate dw 0
+    immediate_output dw 0
+    immediate_width db 0
     current_offset_index db 0
+    current_immediate_index db 0
     command_names   db "sub  "
                     db "aas  "
                     db "das  "
@@ -34,8 +49,8 @@ LOCALS @@
                     db "daa  "
     unknown_command db "nop  "
     command_break db ASCII_CR, ASCII_LF
-    reg_operand db 30 dup (?)
-    reg_mem_operand db 30 dup (?)
+    reg_operand db 0, 30 dup (?)
+    reg_mem_operand db 0, 30 dup (?)
     operand_separator db ", "
     reg_byte     db "al"
     reg_byte_tag db "cl"
@@ -96,6 +111,57 @@ convert_offset proc near
         ret
 convert_offset endp
 
+dump_immediate proc near
+        push cx ax bx
+
+        cmp _width, 1
+        jne @@skip_extension
+        cmp immediate_width, 1
+        jne @@skip_extension
+
+        sar immediate, 8
+    @@skip_extension:
+        mov bl, '0'
+        mov byte ptr ds:[di], bl
+        inc di
+
+        mov ax, word ptr immediate
+        mov al, ah
+        call convert_offset
+        
+        mov bl, al
+        mov byte ptr ds:[di], bl
+        inc di
+
+        mov bl, ah
+        mov byte ptr ds:[di], bl
+        inc di
+
+        cmp _width, 1
+        jne @@exit
+
+        mov ax, word ptr immediate
+        call convert_offset
+        
+        mov bl, al
+        mov byte ptr ds:[di], bl
+        inc di
+
+        mov bl, ah
+        mov byte ptr ds:[di], bl
+        inc di
+    @@exit:
+        mov bl, 'h'
+        mov byte ptr ds:[di], bl
+        inc di
+
+        mov bl, '$'
+        mov byte ptr ds:[di], bl
+
+        pop bx ax cx
+        ret
+dump_immediate endp
+
 dump_offset proc near
         cmp al, 0
         je @@exit
@@ -151,14 +217,20 @@ dump_offset endp
 
 dump_directed_operands proc near
         push si di
+        
+        cmp immediate_output, 0
+        je @@begin
 
+        mov di, immediate_output
+        call dump_immediate
+    @@begin:
         lea si, reg_mem_operand
         lea di, reg_operand
         
-        cmp register, 0FFh
+        cmp byte ptr reg_operand, 0
         je @@reg_mem_only
     
-        cmp reg_mem_operand, 0FFh
+        cmp byte ptr reg_mem_operand, 0
         je @@reg_only
         
         cmp direction, 1
@@ -171,7 +243,7 @@ dump_directed_operands proc near
         lea si, reg_operand
         jmp @@only_one
     @@reg_mem_only:
-        cmp reg_mem_operand, 0FFh
+        cmp byte ptr reg_mem_operand, 0
         je @@exit
         jmp @@only_one
     @@dump:
@@ -283,7 +355,7 @@ dump_command proc near
         lea di, reg_operand
         call decode_operand
     @@register_skip:
-        cmp reg_mem_operand, 0FFh
+        cmp register_memory, 0FFh
         je @@reg_mem_skip
 
         mov ah, register_memory
@@ -309,39 +381,93 @@ dump_command proc near
 
         mov mode, 0FFh
         mov register, 0FFh
+        mov register_memory, 0FFh
         mov direction, 0FFh
         mov _width, 0FFh
         mov command, 0FFh
+        mov immediate, 0
+        mov immediate_output, 0
+        mov immediate_width, 0
         mov state, 0
         mov _offset, 0
         mov current_offset_index, 0
+        mov byte ptr reg_operand, 0
+        mov byte ptr reg_mem_operand, 0
 
         pop cx
         ret
 dump_command endp
 
+schedule_immediate_parse proc near
+        push bx
+        mov state, STATE_IMMEDIATE
+        mov bl, _width
+        mov current_immediate_index, bl
+        cmp immediate_width, 1
+        jne @@exit
+        mov current_immediate_index, 0
+    @@exit:
+        pop bx
+        ret
+schedule_immediate_parse endp
+
 decode_byte proc near
         cmp state, 0
         je @@match_command
+
+        cmp state, STATE_DEEP_2
+        je @@match_command_2
 
         cmp state, STATE_TWO_OPERANDS
         je @@two_operands
 
         cmp state, STATE_READ_OFFSET
         je @@read_offset
+
+        cmp state, STATE_IMMEDIATE
+        je @@read_immediate
     @@read_offset:
-        cmp current_offset_index, 2
-        je @@read_offset_next
+        cmp current_offset_index, 1
+        ja @@read_offset_next
 
         shl _offset, 8
         mov ah, 0
         add _offset, ax
+
+        cmp immediate_output, 0
+        je @@skip_immediate_read_after_offset
+        call schedule_immediate_parse
+        ret
+    @@skip_immediate_read_after_offset:
         call dump_command
         ret
     @@read_offset_next:
         mov ah, 0
         mov _offset, ax
         dec current_offset_index
+
+        ret
+    @@read_immediate:
+        cmp current_immediate_index, 0
+        ja @@read_immediate_next
+
+        push cx
+        mov cx, immediate
+        mov ch, al
+        mov immediate, cx
+        pop cx
+        
+        call dump_command
+        
+        ret
+    @@read_immediate_next:
+        push cx
+        mov cx, immediate
+        mov cl, al
+        mov immediate, cx
+        pop cx
+
+        dec current_immediate_index
 
         ret
     @@two_operands:
@@ -368,6 +494,12 @@ decode_byte proc near
         cmp mode, 10b
         je @@schedule_offset_read
 
+        cmp immediate_output, 0
+        je @@skip_immediate_parse
+        
+        call schedule_immediate_parse
+        ret
+    @@skip_immediate_parse:
         call dump_command
         ret
     @@schedule_offset_read:
@@ -376,12 +508,59 @@ decode_byte proc near
 
         ret
     @@match_command:
+        cmp al, 00111111b
+        je @@aas
+
+        cmp al, 00101111b
+        je @@das
+
+        cmp al, 00110111b
+        je @@aaa
+
+        cmp al, 00100111b
+        je @@daa
+
         mov ah, al
         and ah, 11111100b
         cmp ah, 00101000b
         je @@sub_1
 
-        ; TODO: unrecognized
+        mov ah, al
+        and ah, 11111110b
+        cmp ah, 00101100b
+        je @@sub_2
+
+        mov ah, al
+        and ah, 11111100b
+        cmp ah, 10000000b
+        je @@deep_2
+
+        ; Unrecognized
+        call dump_command
+        ret
+    @@match_command_2:
+        mov ah, al
+        and ah, 00111000b
+        cmp ah, 00101000b
+        je @@sub_3
+
+        ; Unrecognized
+        call dump_command
+        ret
+    @@das:
+        mov command, CMD_DAS
+        call dump_command
+        ret
+    @@aas:
+        mov command, CMD_AAS
+        call dump_command
+        ret
+    @@aaa:
+        mov command, CMD_AAA
+        call dump_command
+        ret
+    @@daa:
+        mov command, CMD_DAA
         call dump_command
         ret
     @@sub_1:
@@ -401,6 +580,42 @@ decode_byte proc near
         mov state, STATE_TWO_OPERANDS
         
         ret
+    @@deep_2:
+        mov state, STATE_DEEP_2
+        ; Save width
+        mov ah, al
+        and ah, 1b
+        mov _width, ah
+
+        ; Save immediate argument size
+        mov ah, al
+        shr ah, 1
+        and ah, 1b
+        mov immediate_width, ah
+
+        ret
+    @@sub_2:
+        ; Save width
+        mov ah, al
+        and ah, 1b
+        mov _width, ah
+        
+        mov register, 0
+
+        mov current_immediate_index, ah
+        mov immediate_output, offset reg_mem_operand
+        mov direction, 1
+
+        ; Set up flags for further processing
+        mov command, CMD_SUB
+        mov state, STATE_IMMEDIATE
+
+        ret
+    @@sub_3:
+        mov command, CMD_SUB
+        mov immediate_output, offset reg_operand
+        mov direction, 0
+        jmp @@two_operands
 decode_byte endp
 
 decode_buffer proc near
